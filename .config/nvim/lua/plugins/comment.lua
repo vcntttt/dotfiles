0baf0de07b6ca99674f3df9b2c2f628cb8fecd4c
Vicente Rivera <vicenterivera225@gmail.com>
2024-01-18 19:35:37 -0300

    nvim powerup

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,15 @@
+return {
+  "numToStr/Comment.nvim",
+  toggler = {
+        ---Línea única
+        line = 'gcc',
+        ---Bloque de líneas
+        block = 'gbc',
+  },
+  opleader = {
+        ---Línea única
+        line = 'gc',
+        ---Bloque de líneas
+        block = 'gb',
+  },
+}
