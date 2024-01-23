601336cd65011531311e773ba86dd23009f63c9d
Vicente Rivera <vicenterivera225@gmail.com>
2024-01-23 19:51:41 -0300

    minor add

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -9,7 +9,7 @@ groupsNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 groupsLabels = ['', "", '', '', '5', '6', '7', ' ', '']
 
 groupsLayouts = ['monadtall', 'max', 'columns', 'columns', 'columns', 'columns', 'columns', 'columns', 'columns']
-
+# icons: "   ", "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ",
 for i in range(len(groupsNames)):
     groups.append(Group(
         name=groupsNames[i],
