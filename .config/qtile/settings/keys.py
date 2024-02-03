0cff87adcd71bd6f8c987e4ef8744680aab4381f
Vicente Rivera <vicenterivera225@gmail.com>
2024-02-03 18:56:19 -0300

    vim movement

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -9,28 +9,28 @@ fileManager = 'nemo'
 
 keys = [
     # Moverse entre ventanas
-    Key([win], 'Left', lazy.layout.left(), desc='Move focus to left'),
-    Key([win], 'Right', lazy.layout.right(), desc='Move focus to right'),
-    Key([win], 'Down', lazy.layout.down(), desc='Move focus down'),
-    Key([win], 'Up', lazy.layout.up(), desc='Move focus up'),
+    Key([win], 'l', lazy.layout.left(), desc='Move focus to left'),
+    Key([win], 'h', lazy.layout.right(), desc='Move focus to right'),
+    Key([win], 'j', lazy.layout.down(), desc='Move focus down'),
+    Key([win], 'k', lazy.layout.up(), desc='Move focus up'),
     Key([win], 'space', lazy.layout.next(),
         desc='Move window focus to other window'),
 
     # Reacomodar ventanas
-    Key([win, 'shift'], 'Left', lazy.layout.shuffle_left(),
+    Key([win, 'shift'], 'l', lazy.layout.shuffle_left(),
         desc='Move window to the left'),
-    Key([win, 'shift'], 'Right', lazy.layout.shuffle_right(),
+    Key([win, 'shift'], 'h', lazy.layout.shuffle_right(),
         desc='Move window to the right'),
-    Key([win, 'shift'], 'Down', lazy.layout.shuffle_down(), desc='Move window down'),
-    Key([win, 'shift'], 'Up', lazy.layout.shuffle_up(), desc='Move window up'),
+    Key([win, 'shift'], 'j', lazy.layout.shuffle_down(), desc='Move window down'),
+    Key([win, 'shift'], 'k', lazy.layout.shuffle_up(), desc='Move window up'),
 
     # Redimensionar Ventanas
-    Key([win, 'control'], 'Left', lazy.layout.grow_left(),
+    Key([win, 'control'], 'l', lazy.layout.grow_left(),
         desc='Grow window to the left'),
-    Key([win, 'control'], 'Right', lazy.layout.grow_right(),
+    Key([win, 'control'], 'h', lazy.layout.grow_right(),
         desc='Grow window to the right'),
-    Key([win, 'control'], 'Down', lazy.layout.grow_down(), desc='Grow window down'),
-    Key([win, 'control'], 'Up', lazy.layout.grow_up(), desc='Grow window up'),
+    Key([win, 'control'], 'j', lazy.layout.grow_down(), desc='Grow window down'),
+    Key([win, 'control'], 'k', lazy.layout.grow_up(), desc='Grow window up'),
 
     # stacks
     Key([alt], 'Tab', lazy.layout.down()),
@@ -59,7 +59,7 @@ keys = [
     Key([win, 'control'], 'r', lazy.reload_config(), desc='Reload the config'),
     Key([win, 'control'], 'q', lazy.shutdown(), desc='Shutdown Qtile'),
     Key([win], 'Tab', lazy.spawn('rofi -show window')),
-    Key([win], 'l', lazy.spawn('betterlockscreen -l')),
+    Key([win, 'shift'], 'l', lazy.spawn('betterlockscreen -l')),
     Key([win], 'BackSpace', lazy.spawn('/home/vrivera/.local/bin/powermenu')),
     Key([win], 'v', lazy.spawn('rofi -modi "clipboard:greenclip print" -show clipboard -run-command "{cmd}"')),
     Key([win, 'shift'], 'e', lazy.spawn('nemo'), desc='launch nemo'),
