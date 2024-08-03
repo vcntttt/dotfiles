577acd424cd0cb4a9b7dc046445263ff698be06f
Vicente Rivera <vicenterivera225@gmail.com>
2024-08-03 17:10:08 -0400

    ajustes scripts

--- .local/bin/layout.sh
+++ .local/bin/layout.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output None-1-1 --off
+xrandr \
+ --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate 165 \
+ --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --rate 144 \
+ --output DP-1 --off \
+ --output HDMI-0 --off \
+ --output DVI-D-0 --off
