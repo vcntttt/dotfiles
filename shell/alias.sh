570fbacbff54a509f77e600107c607b2ddcd2370
Vicente Rivera <vrivera.dev@gmail.com>
2025-03-31 20:53:15 -0300

    reorganizacion de alias y un par de cositas mas

--- shell/alias.sh
+++ shell/alias.sh
@@ -3,25 +3,57 @@ alias logout="qtile cmd-obj -o cmd -f shutdown"
 alias qlog="cat ~/.local/share/qtile/qtile.log"
 alias rmqlog="rm ~/.local/share/qtile/qtile.log"
 alias ff="fastfetch --config default " # --logo-color-1 35 -> morado
-#alias ff="fastfetch --config groups"
+
 ## ----- NVIM ----- ##
 alias qconf="nvim ~/dotfiles/.config/qtile/"
-alias nvimconf="nvim ~/.config/nvim"
+alias nconf="nvim ~/.config/nvim"
 alias ee="nvim ~/dotfiles/.config/espanso/match/base.yml && espanso restart"
+alias nv="nvim ."
+
 ## ------ ARCH ------ ##
 alias grub-reconf="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 alias reflec="sudo reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist"
+alias chx="chmod +x"
 
 ## ------ ZSH ------ ##
 alias zsh="nvim ~/.zshrc && source ~/.zshrc"
 alias elias="nvim ~/dotfiles/shell/alias.sh && source ~/.zshrc"
 alias lalias="bat ~/dotfiles/shell/alias.sh"
 alias sz="source ~/.zshrc"
+
 ## ------ PACMAN ------ ##
 alias pup="sudo pacman -Syu"
 alias yup="yay -Syu --noconfirm"
 alias pi="sudo pacman -S"
 alias pss="pacman -Ss"
+
+## ------ ARCHIVOS ------ ##
+comprimir(){
+    local dir_name=$(basename "$1")
+    tar -czvf "$dir_name".tar.gz "$1"
+}
+
+alias tgz=comprimir
+alias untar="tar -xf"
+alias fm="yazi"
+alias ndot="nvim ~/dotfiles"
+alias ws="windsurf ."
+alias mkdir="mkdir -pv"
+alias cp="cp -i" #i -> confirmacion
+alias mv="mv -i" 
+alias du="du -sh *" # Ver tamaño de archivos
+
+## ------ EZA ------ ##
+alias l="eza --icons=always --color=always --long --no-filesize --git"
+alias ll="eza --icons=always --color=always --long --all --no-user --no-filesize --git"
+alias ls="eza --icons=always -F always"
+alias lg="eza -l --git"
+alias la="eza --icons=always --color=always --long --all --git"
+alias lt="eza --icons=always --color=always --tree --ignore-glob='node_modules'"
+
+## ------ DESARROLLO ------ ##
+alias cleanpm="npx npkill" # cli tool para eliminar paquetes de node_modules
+alias lsc="cloc . --exclude-dir=node_modules,.next,dist,.turbo,.git,vendor --exclude-ext=svg,json,yaml --vcs git" # cuenta la cantidad de archivos
 
 ## ------ GIT ------ ##
 ghiFunction(){
@@ -47,46 +79,33 @@ alias gp="git push"
 # gh-cli
 alias ghinit=ghiFunction
 
-## ------ ARCHIVES ------ ##
-comprimir(){
-    local dir_name=$(basename "$1")
-    tar -czvf "$dir_name".tar.gz "$1"
-}
-
-alias tgz=comprimir
-alias untar="tar -xf"
-alias fm="yazi"
-alias cdot="code ~/dotfiles"
-alias ws="windsurf ."
-alias mkdir="mkdir -pv"
-alias cp="cp -i" #i -> confirmacion
-alias mv="mv -i" 
-alias du="du -sh *" # Ver tamaño de archivos
-alias lsc="cloc . --exclude-dir=node_modules,.next,dist,.turbo,.git,vendor --exclude-ext=svg,json,yaml --vcs git"
-## ------ EZA ------ ##
-alias l="eza --icons=always --color=always --long --no-filesize --git"
-alias ll="eza --icons=always --color=always --long --all --no-user --no-filesize --git"
-alias ls="eza --icons=always -F always"
-alias lg="eza -l --git"
-alias la="eza --icons=always --color=always --long --all --git"
-alias lt="eza --icons=always --color=always --tree --ignore-glob='node_modules'"
-
-## ------ BUN ------ ##
+### ------ BUN ------ ###
 alias br="bun run"
 alias bd="bun run dev"
 alias bi="bun install"
 alias bb="bun run build"
 alias bcn="bunx --bun shadcn@latest"
 
+### ------ DOCKER ------ ###
+alias dcu="docker compose up"
+alias dcd="docker compose down"
+alias penpot="docker compose -p penpot -f ~/docker-apps/penpot/docker-compose.yaml"
+alias tf="terraform"
+
+### ------ MOBILE ------ ###
+alias android="emulator -avd Low_Cost_Device_API_35 &"
+alias easapk="eas build -p android --profile preview"
+
 ## ------ PNPM ------ ##
 alias pr="pnpm run"
 alias ppd="pnpm dev"
 alias ppi="pnpm install"
+
 ## ------ LAMP ------ ##
 alias xmpstart="sudo /opt/lampp/lampp start"
 alias xmpstop="sudo /opt/lampp/lampp stop"
 
-## ------ OTHERS ------ ##
+## ------ OTROS ------ ##
 brilloFunction(){
   if [ "$1" -eq 0 ]; then
     xrandr --output DP-0 --brightness "$2"
@@ -100,19 +119,12 @@ brilloFunction(){
 
 alias mvttf="sudo mv *.ttf /usr/share/fonts/TTF && sudo fc-cache -fv"
 alias brillodk=brilloFunction
+alias typr="nvim -c 'Typr'"
+
+# Informacion
 alias weather="curl wttr.in"
 alias psu="ps aux | grep --color=auto"
 alias ipinfo="ip -br addr"
+# Perifericos
 alias ww="openrgb --profile '/home/vrivera/.config/OpenRGB/todo-blanco.orp'"
 alias dpi='polychromatic-cli -n "Razer Viper V3 HyperSpeed" --dpi'
-alias cleanpm="npx npkill"
-alias typr="nvim -c 'Typr'"
-
-## Docker
-alias dcu="docker compose up"
-alias dcd="docker compose down"
-alias penpot="docker compose -p penpot -f ~/docker-apps/penpot/docker-compose.yaml"
-
-## Mobile
-alias android="emulator -avd Low_Cost_Device_API_35 &"
-alias easapk="eas build -p android --profile preview"
